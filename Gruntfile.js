aca05760ea3e50e28fa30c0838907aac548f6e90
vEnCa-X <venca-x@seznam.cz>
2017-09-06 21:26:21 +0200

    Add Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.initConfig({
+        shell: {
+            composer_self_update: {
+                command: 'composer self-update'
+            },
+            composer_update: {
+                command: 'composer update'
+            },
+            test: {
+                command: 'vendor\\bin\\tester tests -s -p php'
+            },
+            installCodeSoft: {
+                command: 'composer create-project nette/coding-standard nette-coding-standard',
+                command: 'composer create-project nette/code-checker nette-code-checker'
+            },
+            netteCodeChecker: {
+                command: 'php nette-code-checker\\src\\code-checker.php -d src -d tests --short-arrays --strict-types'
+            },
+            netteCodeCheckerFIX: {
+                command: 'php nette-code-checker\\src\\code-checker.php -d src -d tests --short-arrays --strict-types --fix'
+            },
+            netteCodingStandard: {
+                command: 'php nette-coding-standard\\ecs check src tests --config nette-coding-standard\\coding-standard-php71.neon'
+            },
+            netteCodingStandardFIX: {
+                command: 'php nette-coding-standard\\ecs check src tests --config nette-coding-standard\\coding-standard-php71.neon --fix'
+            }
+        }
+    });
+
+    grunt.registerTask('installDependencies', ['shell:init', 'shell:installCodeSoft']);
+    grunt.registerTask('test', ['shell:test']);
+    grunt.registerTask('netteCodeChecker', ['shell:netteCodeChecker']);
+    grunt.registerTask('netteCodeCheckerFIX', ['shell:netteCodeCheckerFIX']);
+    grunt.registerTask('netteCodingStandard', ['shell:netteCodingStandard']);
+    grunt.registerTask('netteCodingStandardFIX', ['shell:netteCodingStandardFIX']);
+
+};
